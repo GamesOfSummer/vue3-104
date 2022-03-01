97d4da00a7bd46c1bccfcb5f3a17b533c6d47c07
Summer T <summer@summertime-development.com>
2022-03-01 10:00:56 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+let getTaco = ref(true);
+const question = ref("");
+
+function getTacoClick() {
+  console.log("clicked");
+  getTaco.value = !getTaco.value;
+}
+
+// watch works directly on a ref
+watch(getTaco, async (newQuestion, oldQuestion) => {
+  console.log(oldQuestion);
+  console.log("getTaco watcher");
+  console.log(newQuestion);
+});
+
+// watch works directly on a ref
+watch(question, async (newQuestion, oldQuestion) => {
+  console.log(oldQuestion);
+  console.log("question watcher");
+  console.log(newQuestion);
+});
+</script>
+
+<template>
+  <div>
+    <button @click="getTacoClick">Click me to order a taco</button>
+    <h2>Where is my taco...</h2>
+  </div>
+
+  <input v-model="question" />
+</template>
